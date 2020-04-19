09262adfc99b46f3631df88c980ff09813d09eb7
timiprojects <timilehinsobola@gmail.com>
2020-04-19 17:40:35 -0600

    first commit

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use(`${baseURL}/category`, categoryRoutes)
 
 //Server static assets
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('/client/build'));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
